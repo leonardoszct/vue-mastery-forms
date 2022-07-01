119e01bdaa03c980a88d08bc2965f27d54d5fc80
Leonardo Caetano <leonardo.caetano@manypets.com>
2022-07-01 12:07:25 -0300

    feat: basic a11y for our components

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,15 +1,34 @@
 <template>
-  <label>{{ label }}</label>
+  <label
+    :for="uuid"
+    v-if="label"
+  >{{ label }}</label>
   <input
+    :id="uuid"
+    :aria-describedby="errorID"
+    :aria-invalid="error ? true : null"
     v-bind="$attrs"
     :placeholder="label"
     class="field"
+    :class="{
+      hasError: error
+    }"
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
   >
+  <p
+    :id="errorID"
+    aria-live="assertive"
+    v-if="error"
+    class="errorMessage"
+  >
+    {{ error }}
+  </p>
 </template>
 
 <script>
+import UniqueID from '../features/UniqueID'
+
 export default {
   name: 'BaseInput',
   props: {
@@ -20,6 +39,21 @@ export default {
     modelValue: {
       type: [String, Number],
       default: ''
+    },
+    error: {
+      type: String,
+      default: ''
+    }
+  },
+  setup () {
+    const uuid = UniqueID().getID()
+    return {
+      uuid
+    }
+  },
+  computed: {
+    errorID () {
+      return this.error ? `${this.uuid}-error` : null
     }
   }
 }
